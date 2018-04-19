ee872cb808ce7727f307bd72c188a4ef1d138c3f
Michael Boroski <mjboroski@gmail.com>
2018-04-19 23:35:46 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word (sentence)
   end
   return holder2.join(" ")
 end
+
+def reverse_each_word (sentence)
+  holder=sentence.split(" ")
+  holder2=[]
+  holder.collect do |word|
+    holder2<<word.reverse
+  end
+  return holder2.join(" ")
+end
